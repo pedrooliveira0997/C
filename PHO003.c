d329fee9baf7899403989449ab0f421dcb6ec81c
pedrooliveira0997 <42185577+pedrooliveira0997@users.noreply.github.com>
2018-08-09 20:02:48 -0300

    Create PHO003.c

---
+++ PHO003.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	
+	float n[10]={15.2,3.5,5.9,10.8,4.5,2.1,6.5,2.2};
+	int i = 0,maior=0,menor=0;
+	
+	for (i=7;i>=0;i--){
+		printf("%.1f\t",n[i]);
+			
+	}
+	
+}
