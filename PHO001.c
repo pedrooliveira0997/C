2037a6513e811a2799d7468560b83628660baa89
pedrooliveira0997 <42185577+pedrooliveira0997@users.noreply.github.com>
2018-08-09 19:59:53 -0300

    PHO001.c
    Estrutura de dados

---
+++ PHO001.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	
+	int n = 4,i=0,soma=0;
+	
+	for (i=0;i<=n;i++){
+		soma = soma + i;
+	}
+	printf("\nO valor da Soma eh %d\n",soma);
+}
